f2ac71c66992d5b69f15b6e3ff1870363a8f9745
Jonathan <jn.gz85@gmail.com>
2018-05-07 13:38:26 +1000

    Revert to original version

--- src/Delivery/Manifest.java
+++ src/Delivery/Manifest.java
@@ -1,49 +1,5 @@
 package Delivery;
 
-import org.junit.Test;
-
-import java.util.HashMap;
-
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.junit.Assert.assertThat;
-
 public class Manifest {
 
-    /**
-     * Testing manifest collection type is of hashmap
-     */
-    @Test
-    public void test_manifest_collection_type(){
-
-        assertThat(
-                "This collection must be of type HashMap whose key is a String and value is an instance of Truck",
-                manifest_object,
-                instanceOf(HashMap.class));
-    }
-
-    /**
-     * Test that manifest takes objects of type Truck
-     */
-    @Test
-    public void test_manifest_value_type() {
-
-        Truck refrigerated_truck = new RefrigeratedTruck();
-        Truck ordinary_truck = new OrdinaryTruck();
-
-        manifest_object.put("refrigerated1", refrigerated_truck);
-        manifest_object.put("ordinary1", ordinary_truck);
-
-
-        // Test that manifest value for refrigerated truck is of type Truck
-        assertThat(
-                "Value object should be of type Item ",
-                manifest_object.get("refrigerated1"),
-                instanceOf(Truck.class));
-
-        // Test that manifest value for ordinary truck is of type Truck
-        assertThat(
-                "Value object should be of type Item ",
-                manifest_object.get("ordinary1"),
-                instanceOf(Truck.class));
-    }
 }
